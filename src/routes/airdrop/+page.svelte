32a4295c164db430e54fa273bc52e097378277e7
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-03-06 11:45:22 +0000

    Update +page.svelte

--- src/routes/airdrop/+page.svelte
+++ src/routes/airdrop/+page.svelte
@@ -1,13 +1,2 @@
-<script lang="ts">
-	let something = false;
-</script>
-
-<div class="mb-4 px-6 mx-auto">
-	<div class="flex flex-wrap -mx-3">
-		<h1>Something</h1>
-	</div>
-
-	<div class="flex flex-wrap mt-6 -mx-3">
-		<h2>content</h2>
-	</div>
-</div>
+<h5>Under development...</h5>
+<p>This feature will allow you to Airdrop tokens to a list of accounts.</p>
