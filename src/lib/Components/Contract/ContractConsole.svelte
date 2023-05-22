098cb8f1525407edc75013e07b095a6c8ac7dfc3
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-05-22 09:45:52 +0100

    Advanved Interactions and fix's

---
+++ src/lib/Components/Contract/ContractConsole.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Card from '$lib/Components/Card/Card.svelte';
+	import { consoleOutput } from '$lib/store';
+	let _consoleOutput = '';
+	consoleOutput.subscribe((value) => {
+		_consoleOutput = value;
+	});
+</script>
+
+<Card size="xl" title="Console" description="Call output" class="mb-20">
+	<textarea
+		class="w-full shadow-md bg-slate-200 border-r-2 rounded-2xl p-4"
+		rows="10"
+		id="contractMethodOutput"
+		bind:value={_consoleOutput}
+		disabled
+	/>
+</Card>
